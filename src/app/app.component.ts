e4c3ff66d017bc4781342ffc94154277b181fa02
veranyagaka <nyagakaver@gmail.com>
2024-08-06 21:37:49 +0300

    Basic setup for todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule],
+
 })
 export class AppComponent {
   title = 'todo';
+  componentTitle ='Veras To Do List';
+  filter: "all"|"active"|"done" ="all";
+  allItems=[
+    {description: "eat", done: true},
+    {description: "pray", done: false},
+    {description: "sleep", done: true},
+    {description: "work", done: false},
+  ];
+  
+  addItem(description: string){
+    if(!description) return;
+
+    this.allItems.unshift({
+      description,
+      done: false,
+    });
+  }
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+  
 }
